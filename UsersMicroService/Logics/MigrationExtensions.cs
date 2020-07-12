f50407b8b1752ed277c3d976c1a13bd6ca36c783
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-07-12 04:45:54 +0430

    Changing Connection String for Local Development

--- UsersMicroService/Logics/MigrationExtensions.cs
+++ UsersMicroService/Logics/MigrationExtensions.cs
@@ -18,7 +18,9 @@ namespace UsersMicroService.Logics
                 try
                 {
                     var db = services.GetRequiredService<T>();
+                    Console.WriteLine("Applying Migrations...");
                     db.Database.Migrate();
+                    Console.WriteLine("Migrations Applied Successfully");
                 }
                 catch (Exception ex)
                 {
